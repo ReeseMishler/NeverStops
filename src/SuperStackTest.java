72891303f43dd288f25925e42d5229ff68a7a824
{Reese <reese.mishler@ou.edu>
2020-10-21 16:56:20 -0500

    Add SuperStackTest

---
+++ src/SuperStackTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SuperStackTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
